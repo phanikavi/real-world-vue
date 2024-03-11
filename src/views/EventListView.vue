eadfe0f5fffaaf7db6a536a5b6084800dc8dd540
Phani <phanikavi@github.com>
2024-03-11 02:42:54 -0400

    API Calls with Axios

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,44 +1,19 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService';
 import EventCard from '@/components/EventCard.vue';
 
-import { ref } from 'vue'
+const events = ref(null)
 
-const events = ref([
-  {
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28, 2022',
-    time: '12:00',
-    petsAllowed: true,
-    organizer: 'Kat Laydee',
-  },
-  {
-    id: 458297,
-    category: 'food',
-    title: 'Community Gardening',
-    description: 'Join us as we tend to the community edible plants.',
-    location: 'Flora City',
-    date: 'March 14, 2022',
-    time: '10:00',
-    petsAllowed: true,
-    organizer: 'Fern Pollin',
-  },
-  {
-    id: 8419988,
-    category: 'sustainability',
-    title: 'Beach Cleanup',
-    description: 'Help pick the trash along the shore.',
-    location: 'Playa Del Carmen',
-    date: 'July 22, 2022',
-    time: '11:00',
-    petsAllowed: true,
-    organizer: 'Carey',
-  },
-])
-
+onMounted(() => {
+  EventService.getEvents()
+  .then((response)=> {
+    events.value = response.data
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+})
 </script>
 
 <template>
